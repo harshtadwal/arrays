a43281d81df1c7828d851f08ab36320ec7e94c9a
harshtadwal <harshtadwal1@gmail.com>
2025-10-02 00:39:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let arr = [1,132,4,54,55,6];
+// console.log(arr.length);
+// console.log(arr);
+// console.log(arr[4]);
+// arr[0] = 4;
+// console.log(arr);
+// console.log(arr.toString());
+// console.log(arr);
+console.log(arr,typeof arr);
+console.log(arr.length);
+
+console.log(arr.join(" and "));
+let numbers = [1,2,3,4,5,6]
+console.log(numbers.splice(1,2));
+console.log(numbers.splice(1,3));
+console.log(numbers.splice(1,3,33,44));
+  
+
+
+
+
---
+++ loop.js
@@ -0,0 +1,27 @@
+let a = [1,4,5,534,54]
+// for (let index = 0; index < a.length; index++) {
+//     const element = a[index];
+//     console.log(element);  
+// } 
+// a.forEach((value,index,arr) => {
+//     console.log(value,index,arr) 
+// })
+let object = {
+    a : 1,
+    b : 2,
+    c : 3
+}
+//  for (const key in object) {
+//     if (!Object.hasOwn(object, key)) continue;
+    
+//     const element = object[key];
+//     console.log(key,element);
+    
+    
+    
+//  }
+ for (const value of a) {
+    console.log(value)
+    
+    
+ }
